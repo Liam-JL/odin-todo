0ce9919f7e5731c0671c7c709679eb55d0f1f4c4
LiamJPOS <paledeepened@gmail.com>
2025-03-09 11:45:26 +0000

    Add initial structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-// import "./styles.css"
-import {TaskRepository, TaskService} from "./model"
+import "./styles.css"
+import {TaskRepository, TaskService, ProjectRepository, ProjectService} from "./model"
+
+const tasks = new TaskService(new TaskRepository);
+tasks.createTask(["Do a thing"])
+tasks.createTask(["Do another thing"])
+console.log(tasks.taskRepo.tasks)
+
+const taskId = tasks.taskRepo.tasks[1].id
+tasks.updateTask(taskId, ["Do many things instead"])
+console.log(tasks.taskRepo.tasks)
+
+
 
 
 
--- src/model.js
+++ src/model.js
@@ -1,12 +1,21 @@
 class Task {
-    constructor(title, project = "inbox", description = null, dueDate = null, priority = false) {
-        this.id = Date.now().toString(36) + Math.floor(Math.pow(10, 12) + Math.random() * 9*Math.pow(10, 12)).toString(36);
+    #id = Date.now().toString(36) + Math.floor(Math.pow(10, 12) + Math.random() * 9*Math.pow(10, 12)).toString(36);
+
+    constructor(title = '', project = "inbox", description = null, dueDate = null, priority = false) {
         this.title = title;
         this.project = project;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
         this.checked = false;
+    }
+
+    set id(newId) {
+        this.#id = newId;
+    }
+
+    get id() {
+        return this.#id;
     }
 }
 
@@ -26,29 +35,55 @@ class Task {
     }
 }
 
-class ProjectRepository {
-    #projects = []; // change to localStorage
-
-    get projects() {
-        return this.#projects;
-    }
-
-    pushProject(project) {
-        this.#projects.push(project)
-    }
-}
-
 export class TaskService {
     constructor(repo) {
         this.taskRepo = repo;
     }
-
+    
     createTask(inputValues) {
         const task = new Task(...inputValues);
         this.taskRepo.saveTask(task);
         return task;
     }
 
+    updateTask(taskId, inputValues = []) {
+        const taskIndex = this.taskRepo.tasks.findIndex((task) => task.id === taskId);
+        const newTask = new Task(...inputValues);
+        newTask.id = taskId;
+        this.taskRepo.tasks[taskIndex] = newTask;
+        return newTask;  
+    } 
+}
+
+export class ProjectRepository {
+    #projects = new Set(); // change to localStorage
+
+    get projects() {
+        return this.#projects;
+    }
+
+    saveProject(project) {
+        this.#projects.add(project)
+    }
+
+    deleteProject(title) {
+        this.#projects.delete(title);
+    }
+}
+
+export class ProjectService {
+    constructor(repo) {
+        this.projectRepo = repo;
+        this.createProject("inbox");
+    }
+
+    createProject(title) {
+        this.projectRepo.saveProject(title);
+    }
+
+    updateProject(oldTitle, newTitle) {
+        
+    }
 }
 
 
