b0b02f74adc3b691da443d9bbbec93ff51647dcf
LiamJPOS <paledeepened@gmail.com>
2025-03-12 11:17:26 +0000

    Style create project modal

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import "./styles.css"
 import {TaskRepository, TaskService, ProjectRepository, ProjectService} from "./model"
 
-const tasks = new TaskService(new TaskRepository);
-tasks.createTask(["Do a thing"])
-tasks.createTask(["Do another thing"])
-console.log(tasks.taskRepo.tasks)
+class AppController {
+    constructor(taskService, projectService) {
+        this.taskService = taskService;
+        this.projectService = projectService;
 
-const taskId = tasks.taskRepo.tasks[1].id
-tasks.updateTask(taskId, ["Do many things instead"])
-console.log(tasks.taskRepo.tasks)
+        //Casche DOM
+        this.addProjectBtn = document.getElementById("addProjectBtn");
+        this.priorityProjectsBtn = document.getElementById("priorityProjectsBtn");
+        this.headerAccountMenu = document.getElementById("headerAccountMenu");
+        this.tabBarProjectsBtn = document.getElementById("tabBarProjectsBtn");
+        this.addTaskBtn = document.getElementById("addTaskBtn");
+        this.tabBarSortBtn = document.getElementById("tabBarSortBtn");
+        this.createProjectModal = document.getElementById("createProjectModal");
 
+        //Event Listeners
+        this.addProjectBtn.addEventListener("click", this.onAddProjectBtn.bind(this));
+        this.priorityProjectsBtn.addEventListener("click", this.onPriorityProjectsBtn.bind(this));
+        this.headerAccountMenu.addEventListener("click", this.onHeaderAccountMenu.bind(this));
+        this.tabBarProjectsBtn.addEventListener("click", this.onTabBarProjectsBtn.bind(this));
+        this.addTaskBtn.addEventListener("click", this.onAddTaskBtn.bind(this));
+        this.tabBarSortBtn.addEventListener("click", this.onTabBarSortBtn.bind(this));
+    }
 
+    //Controller methods
+    onAddProjectBtn() {
+        console.log("Add project button clicked")
+        this.createProjectModal.showModal();
+    }
 
+    onPriorityProjectsBtn() {
+        console.log("Priority project button clicked")
+    }
 
+    onHeaderAccountMenu() {
+        console.log("header account button clicked")
+    }
 
+    onTabBarProjectsBtn() {
+        console.log("tab bar projects button clicked")
+    }
 
+    onAddTaskBtn() {
+        console.log("add task button clicked")
+    }
+
+    onTabBarSortBtn() {
+        console.log("tab bar sort button clicked")
+    }
+}
+
+const taskService = new TaskService(new TaskRepository());
+const projectService = new ProjectService(new ProjectRepository());
+const appController = new AppController(taskService, projectService);
